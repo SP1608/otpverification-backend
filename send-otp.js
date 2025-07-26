d35a7c2ef68237f7ace8ca57c1576507e2cad103
SP1608 <101674645+SP1608@users.noreply.github.com>
2025-07-27 04:25:45 +0530

    Update send-otp.js

--- send-otp.js
+++ send-otp.js
@@ -111,7 +111,9 @@ const sendOTP = async (req, res) => {
   const { email } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port:587,
+    secure:false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
